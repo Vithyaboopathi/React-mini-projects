93494602c8421af36dc89b91c57b69990ba2aef5
Vithyaboopathi <106966313+Vithyaboopathi@users.noreply.github.com>
2026-01-05 23:48:11 +0530

    names changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Greeting from "./components/Greeting.jsx";
 import Counter from "./components/Counter.jsx";
 import HobbiesList from "./components/HobbiesList.jsx";
 import Welcome from "./components/welcome.jsx";
-import Increment from "./components/Increment.jsx";
 import Boolean from "./components/Boolean.jsx";
+import Increment from "./components/Increment";
 import Timer from "./components/Timer.jsx";
 
 function App() {
@@ -22,8 +22,8 @@ function App() {
       <Counter />
       <HobbiesList hobbies={hobbies} />
       <Welcome />
-      <Increment />
       <Boolean />
+      <Increment />
       <Timer />
     </div>
   );
